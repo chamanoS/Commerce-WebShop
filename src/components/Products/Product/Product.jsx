fc36334b0d8391dbb950ec6305d490aa97d6e4ae
chamano sidney <chamano.sr@gmail.com>
2020-12-13 07:22:38 +0200

    adding styles

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {Card, CardMedia, CardContent, CardActions, Typography,IconButton} from '@material-ui/core';
 import {AddShoppingCart} from '@material-ui/icons';
 import classes from '*.module.css';
+import useStyles from './style'
 
 
 const Product = ({product}) => {
+    const classes = useStyles();
+
     return (
       <Card className={classes.root}>
           <CardMedia className={classes.media} image="" title={product.name}/>
@@ -21,6 +24,11 @@ const Product = ({product}) => {
                       {product.description}
               </Typography>
           </CardContent>
+          <CardActions disableSpacing className={classes.cardActions}>
+              <IconButton aria-label="Add to Cart">
+                  <AddShoppingCart/>
+              </IconButton>
+          </CardActions>
       </Card>
     )
 }
